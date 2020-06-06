e7b5060a5985676f4bd32311e617c7701fd00ca0
andrewjcho84 <andrewjcho84@gmail.com>
2020-06-06 12:24:06 -0700

    data dump for initial database tables

--- fairpay_postgres_create.sql
+++ fairpay_postgres_create.sql
@@ -1,10 +1,11 @@
+-- creates tables in our databases with relevant columns, setting their serial _id columns as the primary key
+
 CREATE TABLE public.users
 (
   "_id" serial,
   "linkedin_user_id" varchar,
   "name" varchar,
   "company_id" bigint,
-  "job_title" varchar,
   "salary" bigint,
   "sexuality" varchar,
   "age" integer,
@@ -12,12 +13,12 @@ CREATE TABLE public.users
   "race" varchar,
   "city" varchar,
   "state" varchar,
-  "zipcode" integer,
+  "zipcode" varchar,
   "email" varchar,
   "oauthtoken" varchar,
   "refreshtoken" varchar,
   "expiresin" integer,
-  CONSTRAINT "users_pk" PRIMARY KEY ("_id")
+  CONSTRAINT "users_pk" PRIMARY KEY ("_id"),
 )
 WITH (
   OIDS=FALSE
@@ -26,8 +27,8 @@ WITH (
 CREATE TABLE public.salary
 (
   "_id" serial,
-  "user_id" bigint,
   "company_id" bigint,
+  "job_title" varchar,
   "employee_type" varchar,
   "years_at_company" integer,
   "years_of_experience" integer,
@@ -57,16 +58,15 @@ WITH (
   OIDS=FALSE
 );
 
-ALTER TABLE public.users CONSTRAINT "users_fk0" FOREIGN KEY
-("company_id") REFERENCES public.company
-("_id")
-ALTER TABLE public.users CONSTRAINT "users_fk1" FOREIGN KEY
-("salary") REFERENCES public.salary
-("_id")
+-- creates foreign key links, company table must be filled first, then salary table, and lastly users table
+ALTER TABLE public.users ADD CONSTRAINT "users_fk0" FOREIGN KEY ("company_id") REFERENCES public.company("_id");
+ALTER TABLE public.users ADD CONSTRAINT "users_fk1" FOREIGN KEY ("salary") REFERENCES public.salary("_id");
 
-ALTER TABLE public.salary CONSTRAINT "salary_fk0" FOREIGN KEY
-("user_id") REFERENCES public.users
-("_id")
-ALTER TABLE public.salary CONSTRAINT "salary_fk1" FOREIGN KEY
-("company_id") REFERENCES public.company
-("_id")
+ALTER TABLE public.salary ADD CONSTRAINT "salary_fk0" FOREIGN KEY ("company_id") REFERENCES public.company("_id");
+
+-- add default test data starting with company table
+INSERT INTO public.company VALUES (1, 'Codesmith-LLC', 'Codesmith', 'Venice', 'Software Engineering Boot Camp', 'West Coast', '90291');
+
+INSERT INTO public.salary VALUES (1, 1, 'Resident', 'Full-Time', 1, 1, 100000, 0, 0, 0, 'yes');
+
+INSERT INTO public.users VALUES (1, 'andrew-cho-37990193', 'Andrew Cho', 1, 1, 'Heterosexual', 36, 'male', 'asian', 'Sherman Oaks', 'California', 'andrewjcho84@gmail.com', 'oauthtoken1', 'refreshtoken1', 10000, '91411')
