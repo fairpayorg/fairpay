1f93e92f4e8e0fb6318c676be8ff0600087e0d66
Tyler Sullberg <tysullberg@gmail.com>
2020-06-06 17:29:36 -0700

    Completed unstyled components for log-in page and first 2 steps of new user flow

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -4,7 +4,12 @@ import React from "react";
 function Login(props) {
   //   console.log("props vs index", props.value, props.index);
 
-  return <div>insde Login</div>;
+  return (<div>
+    <h1>Be an agent of change</h1>
+    <h2>Securely submit your salary information to understand if you or your coworkers are being descriminated against</h2>
+    {/* TODO: link to LinkedIn OAuth*/}
+    <button>Sign in with LinkedIn</button>
+    </div>);
 }
 
 export default Login;
