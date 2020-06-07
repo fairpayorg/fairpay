605dc671acac247f04549673231bfd146bb8a6ea
skang1004 <saejin.kang1004@gmail.com>
2020-06-07 12:45:48 -0700

    each company comparison display only renders when clicked from tab bar

--- client/components/Race_Company.jsx
+++ client/components/Race_Company.jsx
@@ -4,12 +4,11 @@ import { Container } from "@material-ui/core";
 function Race(props) {
   return (
     <React.Fragment>
-      <div
-        hidden={props.value !== props.index || props.view === 1}
-        className="data_display_div"
-      >
-        <h1>Inside race comparisons</h1>
-        <h1>{props.graph}</h1>
+      <div hidden={props.value !== props.index || props.view === 1}>
+        <div className="data_display_div">
+          <h1>Inside race comparisons</h1>
+          <h1>{props.graph}</h1>
+        </div>
       </div>
     </React.Fragment>
   );
