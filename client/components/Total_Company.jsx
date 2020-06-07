ea8fdea48549d83eaa381abfa819ba2b5aad1c8a
skang1004 <saejin.kang1004@gmail.com>
2020-06-07 12:34:26 -0700

    styling changes

--- client/components/Total_Company.jsx
+++ client/components/Total_Company.jsx
@@ -5,8 +5,23 @@ function Total(props) {
   return (
     <React.Fragment>
       <div hidden={props.value !== props.index || props.view === 1}>
-        <h1>Inside Total comparisons</h1>
-        <h1>{props.graph}</h1>
+        <div className="data_display_div">
+          <h1>Inside Total comparisons</h1>
+          <h1>Inside Total comparisons</h1>
+          <h1>Inside Total comparisons</h1>
+          <h1>Inside Total comparisons</h1>
+          <h1>Inside Total comparisons</h1>
+          <h1>Inside Total comparisons</h1>
+          <h1>Inside Total comparisons</h1>
+          <h1>Inside Total comparisons</h1>
+          <h1>Inside Total comparisons</h1>
+          <h1>Inside Total comparisons</h1>
+          <h1>Inside Total comparisons</h1>
+          <h1>Inside Total comparisons</h1>
+          <h1>Inside Total comparisons</h1>
+          <h1>Inside Total comparisons</h1>
+          <h1>Inside Total comparisons</h1>
+        </div>
       </div>
     </React.Fragment>
   );
