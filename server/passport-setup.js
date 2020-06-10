14250cf6dacda8acfd603d69499d3d443ce62401
crperezt <crperez@gmail.com>
2020-06-09 19:52:47 -0700

    refactored for adding user_id column to salary table

--- server/passport-setup.js
+++ server/passport-setup.js
@@ -44,7 +44,7 @@ passport.use(new LinkedInStrategy({
   async function executeQuery() {
     const user = await db.query(getUserQuery, [profile.id]);
     if (user.rows.length === 0) {
-      const addNewUser = await db.query(addNewUserQuery, [profile.id, profile.displayName, profile.emails[0].value, profile.photos[0].value])
+      const addNewUser = await db.query(addNewUserQuery, [profile.id, profile.displayName, profile.emails[0]? profile.emails.value : 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png', profile.photos[0].value])
       const newUser = await db.query(getUserQuery, [profile.id]);
       done(null, profile);
     } else {
