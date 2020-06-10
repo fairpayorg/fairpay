d1b7cdc8f1235948dbc5e29e7055c437dcbd797c
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-06-10 15:08:25 -0700

    Destructure props and fix keys

--- client/components/CompanyComparison.jsx
+++ client/components/CompanyComparison.jsx
@@ -1,12 +1,20 @@
-import React, { useState, useEffect } from "react";
-import { Container, Tabs, Tab, Box, AppBar } from "@material-ui/core";
-import Race_Company from "./Race_Company.jsx";
-import Age_Company from "./Age_Company.jsx";
-import Gender_Company from "./Gender_Company.jsx";
-import Total_Company from "./Total_Company.jsx";
+import React, { useState } from 'react';
+import { Container, Tabs, Tab, Box } from '@material-ui/core';
+import Race_Company from './Race_Company.jsx';
+import Age_Company from './Age_Company.jsx';
+import Gender_Company from './Gender_Company.jsx';
+import Total_Company from './Total_Company.jsx';
 
-function CompanyComparison(props) {
-  const [value, setValue] = useState(0);
+function CompanyComparison({
+  view,
+  index,
+  allNames,
+  aggregateList,
+  raceList,
+  ageList,
+  genderList,
+}) {
+  const [value, setValue] = useState(3);
 
   const changeGraph = (e, newValue) => {
     setValue(newValue);
@@ -15,7 +23,7 @@ function CompanyComparison(props) {
   return (
     <React.Fragment>
       <Container id="company_container">
-        <div hidden={props.view !== props.index} id="company_comparison_div">
+        <div hidden={view !== index} id="company_comparison_div">
           <Tabs
             orientation="vertical"
             variant="scrollable"
@@ -33,35 +41,25 @@ function CompanyComparison(props) {
 
       <Box>
         <Total_Company
-          allNames={props.allNames}
-          aggregateList={props.aggregateList}
-          view={props.view}
+          allNames={allNames}
+          aggregateList={aggregateList}
+          view={view}
           value={value}
           index={0}
         />
       </Box>
       <Box>
-        <Race_Company
-          raceList={props.raceList}
-          view={props.view}
-          value={value}
-          index={1}
-        />
+        <Race_Company raceList={raceList} view={view} value={value} index={1} />
       </Box>
       <Box>
-        <Age_Company
-          view={props.view}
-          value={value}
-          index={2}
-          ageList={props.ageList}
-        />
+        <Age_Company view={view} value={value} index={2} ageList={ageList} />
       </Box>
       <Box>
         <Gender_Company
-          view={props.view}
+          view={view}
           value={value}
           index={3}
-          genderList={props.genderList}
+          genderList={genderList}
         />
       </Box>
     </React.Fragment>
--- client/components/IndividualComparison.jsx
+++ client/components/IndividualComparison.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from 'react';
 import {
   Container,
   Table,
@@ -8,36 +8,27 @@ import {
   TableContainer,
   TableHead,
   TableRow,
-} from "@material-ui/core";
+} from '@material-ui/core';
 
-function IndividualComparison(props) {
+function IndividualComparison({
+  view,
+  index,
+  allNames,
+  allAges,
+  allBaseSalary,
+  allGenders,
+  allTypes,
+  allSexes,
+  allYrsCompany,
+  allYrsExperience,
+}) {
   // need to write logic that loops through the data we get back from the fetch request and renders
   // all the employee data who work at the same company with the same title
-
-  const [name, setName] = useState(props.name);
-  const [company, setCompany] = useState(props.company);
-  const [jobTitle, setJobTitle] = useState(props.jobTitle);
-  const [sexuality, setSexuality] = useState(null);
-  const [age, setAge] = useState(null);
-  const [gender, setGender] = useState();
-  const [race, setRace] = useState();
-  // salary vs hourly employee
-  const [employeeType, setEmployeeType] = useState();
-  // years of experience in field/position
-  const [yrsExperience, setYrsExperience] = useState();
-  // years at current company
-  const [yrsCompany, setYrsCompany] = useState();
-  const [baseSalary, setBaseSalary] = useState();
-  const [annualBonus, setAnnualBonus] = useState();
-  // total invested and uninvested
-  const [stockOptions, setStockOptions] = useState();
-  const [signingBonus, setSigningBonus] = useState();
-  const [ftStatus, setFtStatus] = useState();
 
   return (
     <React.Fragment>
       <Container>
-        <div hidden={props.view !== props.index} id="individual_comparison_div">
+        <div hidden={view !== index} id="individual_comparison_div">
           <TableContainer component={Paper}>
             <Table>
               <TableHead>
@@ -53,23 +44,17 @@ function IndividualComparison(props) {
                 </TableRow>
               </TableHead>
               <TableBody>
-                {props.allNames.map((key, i) => {
+                {allNames.map((key, i) => {
                   return (
-                    <TableRow>
+                    <TableRow key={i}>
                       <TableCell>{key}</TableCell>
-                      <TableCell align="right">{props.allAges[i]}</TableCell>
-                      <TableCell align="right">
-                        ${props.allBaseSalary[i]}
-                      </TableCell>
-                      <TableCell align="right">{props.allGenders[i]}</TableCell>
-                      <TableCell align="right">{props.allTypes[i]}</TableCell>
-                      <TableCell align="right">{props.allSexes[i]}</TableCell>
-                      <TableCell align="right">
-                        {props.allYrsCompany[i]}
-                      </TableCell>
-                      <TableCell align="right">
-                        {props.allYrsExperience[i]}
-                      </TableCell>
+                      <TableCell align="right">{allAges[i]}</TableCell>
+                      <TableCell align="right">${allBaseSalary[i]}</TableCell>
+                      <TableCell align="right">{allGenders[i]}</TableCell>
+                      <TableCell align="right">{allTypes[i]}</TableCell>
+                      <TableCell align="right">{allSexes[i]}</TableCell>
+                      <TableCell align="right">{allYrsCompany[i]}</TableCell>
+                      <TableCell align="right">{allYrsExperience[i]}</TableCell>
                     </TableRow>
                   );
                 })}
